cbe90740fe06259ea61ebea9a78c259eb719d868
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2024-01-25 10:20:54 +0200

    finish styling desktop with hover effects

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    screens: {
+      'lg': {'max': '1440px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'sm': {'max': '375px'},
+      // => @media (max-width: 639px) { ... }
+    },
     fontFamily: {
       "Open Sans": ["Open Sans", "sans-serif"],
       Poppins: ["Poppins", "sans-serif"],
